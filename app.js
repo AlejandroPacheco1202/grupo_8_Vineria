3777444f5386bd361669e24bb6a7d2ce49469f12
Alejandro Pacheco1202 <alep1202@gmail.com>
2022-07-04 07:20:03 -0300

    modificadas las vistas a EJS

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require ('path');
 const publicPath=path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
+app.set('view engine','ejs');
+app.use(express.static('public'));
 
-const mainRouter = require('./routers/mainRouter');
-app.use('/', mainRouter);
+const mainRoutes= require('./routes/mainRoutes');
+app.use('/', mainRoutes);
 
 
 app.listen(3000, () => {
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 const mainController={
-    index: (request, response)=>{response.sendFile(path.join(__dirname,'../views/index.html'))},
-    producto:(request, response)=>{response.sendFile(path.join(__dirname,'../views/producto.html'))},
-    carrito:(request, response)=>{response.sendFile(path.join(__dirname,'../views/carrito.html'))},
-    login:(request, response)=>{response.sendFile(path.join(__dirname,'../views/login.html'))},
-    register:(request, response)=>{response.sendFile(path.join(__dirname,'../views/register.html'))} 
+    index: (request, response)=>{
+        response.render('index')
+    },
+    producto:(request, response)=>{
+        response.render('producto')
+    },
+    carrito:(request, response)=>{
+        response.render('carrito')
+    },
+    login:(request, response)=>{
+        response.render('login')
+    },
+    register:(request, response)=>{
+        response.render('register')
+    } 
 };
 
 
